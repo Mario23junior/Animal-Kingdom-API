ae548af19c86ddaf6288af0851789863a6bca71c
Mario23junior <mariojunior3251@gmail.com>
2021-04-02 04:18:02 -0300

    Iniciando projeto de gerenciamento de dados de animais

---
+++ src/test/java/com/project/AnimalKingdom/AnimalKingdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AnimalKingdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalKingdomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
