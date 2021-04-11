f7dbb6cb3f227a4d8834b83127663adc631024ca
Mario23junior <mariojunior3251@gmail.com>
2021-04-11 03:31:26 -0300

    Listando informacaoes por id

--- src/main/java/com/project/AnimalKingdom/Controller/ComplementInformaController.java
+++ src/main/java/com/project/AnimalKingdom/Controller/ComplementInformaController.java
@@ -3,11 +3,14 @@ package com.project.AnimalKingdom.Controller;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.AnimalKingdom.model.ComplementInforma;
 import com.project.AnimalKingdom.repositorys.ComplementInformaRepository;
@@ -26,6 +29,13 @@ public class ComplementInformaController {
    @ResponseStatus(HttpStatus.CREATED)
    public ComplementInforma save(@RequestBody @Valid ComplementInforma complementInforma) {
 	   return complementInformaRepository.save(complementInforma);
+   }
+   
+   @GetMapping("/{id}")
+    public ComplementInforma findId(@PathVariable Long id) {
+	   return complementInformaRepository 
+			     .findById(id)
+			     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT,"Informação não encontrada"));
    }
    
 }
