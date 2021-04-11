1622f621ce5342bdf627c0b05f323a91daf490bb
Mario23junior <mariojunior3251@gmail.com>
2021-04-11 02:54:17 -0300

    Salvando dados no banco e inserindo informacoes na base

--- src/main/java/com/project/AnimalKingdom/Controller/ComplementInformaController.java
+++ src/main/java/com/project/AnimalKingdom/Controller/ComplementInformaController.java
@@ -1,5 +1,31 @@
 package com.project.AnimalKingdom.Controller;
 
-public class ComplementInformaController {
+import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.AnimalKingdom.model.ComplementInforma;
+import com.project.AnimalKingdom.repositorys.ComplementInformaRepository;
+
+@RestController
+@RequestMapping("info/comple")
+public class ComplementInformaController {
+    
+   ComplementInformaRepository complementInformaRepository;
+   
+   public ComplementInformaController(ComplementInformaRepository complementInformaRepository) {
+	    this.complementInformaRepository = complementInformaRepository;
+   }
+   
+   @PostMapping("/")
+   @ResponseStatus(HttpStatus.CREATED)
+   public ComplementInforma save(@RequestBody @Valid ComplementInforma complementInforma) {
+	   return complementInformaRepository.save(complementInforma);
+   }
+   
 }
