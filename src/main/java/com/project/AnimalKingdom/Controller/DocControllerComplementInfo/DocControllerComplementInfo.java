9022b9a2ea20764eefb49af3af68d896767793c9
Mario23junior <mariojunior3251@gmail.com>
2021-04-14 01:40:47 -0300

    Inserindo anotaçãoes de documentacao no metodo salvar

--- src/main/java/com/project/AnimalKingdom/Controller/DocControllerComplementInfo/DocControllerComplementInfo.java
+++ src/main/java/com/project/AnimalKingdom/Controller/DocControllerComplementInfo/DocControllerComplementInfo.java
@@ -1,5 +1,23 @@
 package com.project.AnimalKingdom.Controller.DocControllerComplementInfo;
 
-public interface DocControllerComplementInfo {
+import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.project.AnimalKingdom.model.ComplementInforma;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+public interface DocControllerComplementInfo {
+    
+	
+	@ApiOperation(value = "Salvando animais no banco de dados")
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "sucesso"),
+			@ApiResponse(code = 500, message = "erro ao salvar dados")
+			
+	})
+	   public ComplementInforma save(@RequestBody @Valid ComplementInforma complementInforma);
 }
