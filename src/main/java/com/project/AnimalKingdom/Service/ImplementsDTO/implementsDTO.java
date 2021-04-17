3cfcb8d62d80e9a684b488e79d4f751384588c79
Mario23junior <mariojunior3251@gmail.com>
2021-04-17 03:09:09 -0300

    Criando classe a ser implementada

---
+++ src/main/java/com/project/AnimalKingdom/Service/ImplementsDTO/implementsDTO.java
@@ -0,0 +1,15 @@
+package com.project.AnimalKingdom.Service.ImplementsDTO;
+
+import java.util.List;
+
+import com.project.AnimalKingdom.ModelDTO.AnimalDTO;
+import com.project.AnimalKingdom.Service.ServiceDTOInplements;
+
+public class implementsDTO implements ServiceDTOInplements {
+
+	@Override
+	public List<AnimalDTO> listAll() {
+ 		return null;
+	}
+
+}
---
+++ src/main/java/com/project/AnimalKingdom/Service/ServiceDTOInplements.java
@@ -0,0 +1,10 @@
+package com.project.AnimalKingdom.Service;
+
+import java.util.List;
+
+import com.project.AnimalKingdom.ModelDTO.AnimalDTO;
+
+public interface ServiceDTOInplements {
+    
+	List<AnimalDTO> listAll();
+}
